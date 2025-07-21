33d41e0b64c3c2f61ec6f94650dffdc9ccb60bda
shetkir <ll901mauil.ru@gmail.com>
2025-07-21 18:26:24 +0400

    modified:   printsamara/src/main/java/com/example/printsamara/PrintsamaraApplication.java

--- printsamara/src/main/java/com/example/printsamara/PrintsamaraApplication.java
+++ printsamara/src/main/java/com/example/printsamara/PrintsamaraApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrintsamaraApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(PrintsamaraApplication.class, args);
 
 		System.out.println("                                 @:@                                 ");
