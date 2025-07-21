f623136698f05855484d7f53dbc58557a7b5ca57
shetkir <ll901mauil.ru@gmail.com>
2025-07-21 18:18:59 +0400

    new file:   printsamara/.gitattributes 	new file:   printsamara/.gitignore 	new file:   printsamara/.mvn/wrapper/maven-wrapper.properties 	new file:   printsamara/mvnw 	new file:   printsamara/mvnw.cmd 	new file:   printsamara/pom.xml 	new file:   printsamara/src/main/java/com/example/printsamara/PrintsamaraApplication.java 	new file:   printsamara/src/main/resources/application.properties 	new file:   printsamara/src/test/java/com/example/printsamara/PrintsamaraApplicationTests.java

---
+++ printsamara/src/test/java/com/example/printsamara/PrintsamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.printsamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintsamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
